13eaff8bbe41f5e0c3213e03a3eecc07a490253b
Kalen Meiirzhan <kalen.mm@mail.ru>
2022-05-10 16:02:04 +0600

    new

--- library/urls.py
+++ library/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 app_name = 'library'
 urlpatterns = [
-
+    path('', views.IndexView, name='index'),
+    path('login/', views.LoginView, name='login'),
+    path('register/', views.RegisterView, name='register'),
 ]
--- library/views.py
+++ library/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def IndexView(request):
+    template_name = "library/index.html"
+    return render(request, template_name)
+
+
+def LoginView(request):
+    template_name = "library/login.html"
+    return render(request, template_name)
+
+
+def RegisterView(request):
+    template_name = "library/register.html"
+    return render(request, template_name)
+
